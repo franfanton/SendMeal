6f78b35645f619207bad04c336e096ff69bb6c13
franfanton <franfanton92@gmail.com>
2020-09-29 19:26:49 -0300

    acomodamos los archivos y agregamos nueva vista principal

---
+++ app/src/main/java/com/example/lab1/MainActivity2.java
@@ -0,0 +1,12 @@
+package com.example.lab1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vistaprincipal);
+    }
+}
