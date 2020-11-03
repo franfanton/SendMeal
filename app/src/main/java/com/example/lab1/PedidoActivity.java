0814f5469fc501c5bb4adc30a72b716690421aae
franfanton <franfanton92@gmail.com>
2020-11-03 20:50:07 -0300

    punto 4 de notificacion al dar alta el Pedido

--- app/src/main/java/com/example/lab1/PedidoActivity.java
+++ app/src/main/java/com/example/lab1/PedidoActivity.java
@@ -2,10 +2,16 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,12 +21,14 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import com.example.lab1.model.MyNotificationPublisher;
+
 import com.example.lab1.model.Pedido;
 
-import java.util.ArrayList;
-
 public class PedidoActivity extends AppCompatActivity {
+    // NOTIFICACION
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    private final static String default_notification_channel_id = "default";
+    // FIN NOTIFICACION
     private EditText correoPedidoNuevo, direccionPedidoNuevo;
     private RadioButton botonEnvioPedido,botonTakeawayPedido;
   //  BotonAsyncTask botonAsyncTask;
@@ -77,6 +85,12 @@ public class PedidoActivity extends AppCompatActivity {
                     //i.putExtra("tipoEnvio",tipoEnvio);
                     //startActivity(i);
                     //startActivityForResult(i, CODIGO_ACTIVIDAD);
+
+                    // ALTA DE NOTIFICACION
+                    String tittle = "Send Mail";
+                    String content = "Pedido cargado con exito!";
+                    int delay = 5;
+                    scheduleNotification(getNotification(content, tittle), delay);
 
                 }
             }
@@ -148,5 +162,29 @@ public class PedidoActivity extends AppCompatActivity {
 
         }
         return true;
+    }
+
+    // FUNCIONES NOTIFICACION
+
+    private void scheduleNotification(Notification notification, int delay) {
+
+        Intent notificationIntent = new Intent(this, MyNotificationPublisher.class);
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION, notification);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content, String tittle) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, default_notification_channel_id);
+        builder.setContentTitle(!tittle.isEmpty() ? tittle : "Scheduled Notification");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setAutoCancel(true);
+        builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+        return builder.build();
     }
 }
